007008da39ff00233fdcde1346d69640b16f2436
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-12-23 09:07:16 +0100

    Final

---
+++ src/components/devinettes/pointFinal/Last.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { Container , Form ,  Button} from "react-bootstrap";
+import { useForm } from 'react-hook-form';
+
+function Last() {
+
+    const {
+        register,
+        handleSubmit,
+      } = useForm();
+
+    const [show , setShow] = useState("")
+
+    const handleResponse = (data) => {
+        const answer = data.reponse.toLowerCase()
+
+        if(answer === 'incorrect') {
+            setShow(true)
+        }
+        else {
+            setShow(false)
+        }
+    }
+
+    return (
+        <Container className="mt-5">
+            <Form className="mb-3"  onSubmit={handleSubmit(handleResponse)}>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label className="fs-3 fw-medium">Reponse</Form.Label>
+                    <Form.Control type="text" placeholder="reponse" {...register('reponse')}/>
+                </Form.Group>
+                {show === false && <p className="alert alert-danger">Votre réponse est incorrect</p>}
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            {
+                show && 
+                <div className="d-flex flex-column align-items-center text-center fs-3 mt-5">
+                   Je suis le premier et le dernier. L'alpha et l'omega
+                </div>
+            }
+        </Container>
+    )
+}
+
+export default Last
