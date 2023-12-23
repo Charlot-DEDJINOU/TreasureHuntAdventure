ce580ff8a51b4199267f6e7439d3d176d36b01f6
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2023-12-23 03:28:40 +0100

    new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import '../styles/style.css'
 import Header from './header/Header';
-import Gbios from './devinettes/Gbios';
-import Auf from './devinettes/Auf';
+import Confucius from './devinettes/Confucius';
+// import Moocs from './devinettes/Moocs';
+// import ViceRectorat from './devinettes/ViceRectorat';
+// import Station from './devinettes/Station';
+// import Gbios from './devinettes/Gbios';
+// import Auf from './devinettes/Auf';
+// import Enam from './devinettes/Enam';
 
 function App() {
   return (
    <React.StrictMode>
       <Header />
       {/* <Gbios /> */}
-      <Auf/>
+      {/* <Auf/> */}
+      {/* <Station /> */}
+      {/* <ViceRectorat /> */}
+      {/* <Moocs /> */}
+      {/* <Enam /> */}
+      <Confucius />
    </React.StrictMode>
   );
 }
