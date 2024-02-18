371286d4d3f2031f16e168a84dc05b1aa6ae9aec
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-18 17:10:29 +0100

    add presentation page

--- src/components/Exemple.js
+++ src/components/Exemple.js
@@ -1,27 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export default function Exemple() {
- 
-  const [count, setCount] = useState(0);
-  const [message, setMessage] = useState("Nombre pair");
-
-  const charlot = () => {
-    
-    if((count + 1) % 2 === 0)
-        setMessage(count + 1 + " Nombre pair")
-    else 
-        setMessage(count + 1 + " Nombre impaire")
-
-    setCount(count + 1)
-  }
 
   return (
-    <div className='d-flex flex-column justify-content-center align-items-center mt-5'>
-       <div className='w-50 d-flex justify-content-around'>
-            <button className='btn btn-primary' onClick={charlot}>+</button>
-            <button className='btn btn-info'>{ count }</button>
-            <button className='btn btn-success'>{ message }</button>
-       </div>
+    <div className='d-flex flex-column justify-content-center align-items-center w-100 fs-3 fw-medium text-center' style={{ height : "85vh"}}>
+       Bienvenue à la chasse au trésor. Arriverez-vous à trouver le trésor caché ?
     </div>
   );
 }
