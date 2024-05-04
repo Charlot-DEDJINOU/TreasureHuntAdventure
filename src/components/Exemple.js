e22b412ff495ee12a451475cc81520dbba24cd24
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-05-04 23:29:04 +0100

    remove h

--- src/components/Exemple.js
+++ src/components/Exemple.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Exemple() {
 
   return (
-    <div className='d-flex flex-column justify-content-center align-items-center w-100 fs-3 fw-medium text-center' style={{ height : "85vh"}}>
+    <div className='d-flex flex-column justify-content-center align-items-center w-100 fs-3 fw-medium text-center'>
        Bienvenue à la chasse au trésor. Arriverez-vous à trouver le trésor caché ?
     </div>
   );
